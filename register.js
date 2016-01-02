eb5ca15184c738bf0676e98f005857e119ebf837
Donald Atkinson <fuzzicallogic@outlook.com>
2016-01-02 09:48:30 -0700

    Initial Element Setup

---
+++ register.js
@@ -0,0 +1,32 @@
+(function registerElement() {
+
+	Polymer({
+		is: 'forge-reloader',
+
+		created: onElementCreated,
+		attached: onElementAttached,
+		ready: onElementReady,
+		detached: onElementDetached,
+
+		properties: {
+		},
+
+	});
+
+	function onElementCreated() {
+
+	}
+
+	function onElementAttached() {
+
+	}
+
+	function onElementReady() {
+
+	}
+
+	function onElementDetached() {
+
+	}
+
+}) ();
